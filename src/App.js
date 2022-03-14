846132a92097bebb972e14e657809a69e471d68f
David Best <Best-D4@ulster.ac.uk>
2022-03-14 19:33:50 +0000

    Pass in props, added ExpenseDate and Expenses Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import ExpenseItem from './components/ExpenseItem'
+import Expenses from './components/Expenses';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Home Insurance',
+      amount: 50.45,
+      date: new Date(2022, 2, 21),
+    },
+    { id: 'e2', title: 'Rent', amount: 600.0, date: new Date(2022, 2, 24) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2022, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'Electricity',
+      amount: 75.0,
+      date: new Date(2022, 2, 30),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem/>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
