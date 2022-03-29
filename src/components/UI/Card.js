a9ace0dd23e8dba1dfe972518530dcb1efe390c0
David Best <Best-D4@ulster.ac.uk>
2022-03-29 17:15:31 +0100

    Add Styled Components

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,5 +1,11 @@
 import React from 'react';
+// import styled from 'styled-components';
 import './Card.css';
+
+// const Card = styled.div`
+//   border-radius: 12px;
+//   box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);
+// `;
 
 const Card = (props) => {
   const classes = 'card ' + props.className;
